efe65ea24cef4790180dd5a1e1463c8e2d9f3f2f
nitesh <niteshmuz06@gmail.com>
2025-03-19 20:35:36 +0530

    paly with variables

---
+++ code/day2.py
@@ -0,0 +1,28 @@
+int_x = 45
+float_y = 34.56
+
+print(type(int_x))
+print(type(float_y))
+
+name = "Nitesh"
+print(type(name))
+
+is_active = True
+print(type(is_active))
+numbers = [1,5,7,9,2,56,56]
+print(type(numbers))
+
+_tuple = (3,6)
+print(type(_tuple))
+
+# Tasks
+# create a variable to store age
+# create a list to store favorite movies)
+# creata a dictionary with key values pairs name,age,city
+
+age = 45;
+print(age)
+list_of_favorite_movies = ['Gaddar','chaava','Teri kasam']
+print(list_of_favorite_movies)
+dic_details={"name": "Nitesh","age": 35,"city": "Muzaffarpur"}
+print(dic_details)
